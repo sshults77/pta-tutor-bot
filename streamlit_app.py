fe6149be0d8008d8c3990dce3f1e598ca4a8e57e
sshults77 <sshults@south.edu>
2025-06-07 01:05:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ if "messages" not in st.session_state:
         st.session_state.messages = []
 
     # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
+        for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
