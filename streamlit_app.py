6bc9086074fda0a80bea5f9ce2688cd1120eb41e
sshults77 <sshults@south.edu>
2025-06-16 21:56:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
+import streamlit as st
 import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
 st.write("streamlit-authenticator version:", stauth.__version__)
+
 import yaml
 from yaml.loader import SafeLoader
 
